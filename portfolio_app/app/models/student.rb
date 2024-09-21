2e3dc5d075aac0e35ce8ceef8cb81c86c5dc5bf6
Kyle Zimmerman <kzimme15@msudenver.edu>
2024-09-21 13:22:49 -0600

    finished adding validations and tests

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -2,6 +2,10 @@ class Student < ApplicationRecord
 
     validates :first_name, presence: true
     validates :last_name, presence: true
+    validates :major, presence: true
+    validates :graduation_date, presence: true
+    validates :school_email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
     
 
+    #has_one_attached :avatar
 end
--- portfolio_app/test/models/student_test.rb
+++ portfolio_app/test/models/student_test.rb
@@ -7,16 +7,60 @@ class StudentTest < ActiveSupport::TestCase
 
 require "test_helper"
 
+  # test 1
   test "Should rais an error when saving student without first name" do
     assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(last_name: "Burger", school_email: "burger12@msudenver.edu", major: "CS")
+      Student.create!(last_name: "Burger", school_email: "burger12@msudenver.edu", major: "CS", graduation_date: "2025-06-01")
     end
   end
 
+  #test 2
   test "Should rais an error when saving student without last name" do
     assert_raises ActiveRecord::RecordInvalid do
-      Student.create!(first_name: "Burger", school_email: "burger12@msudenver.edu", major: "CS")
+      Student.create!(first_name: "Burger", school_email: "burger12@msudenver.edu", major: "CS", graduation_date: "2025-06-01")
     end
   end
 
+  # test 3
+  test "Should rais an error when saving student without a major" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "Ham", last_name: "Burger", school_email: "major@msudenver.edu", graduation_date: "2025-06-01")
+    end
+  end
+
+  # test 4
+  test "Should rais an error when saving student without a graduation date" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "Ham", last_name: "Burger", school_email: "gradDate@msudenver.edu", major: "CS")
+    end
+  end
+
+  # test 5
+  # uses an email that is already part of a fixture
+  test "Should rais an error when saving student with an inuse email" do
+    assert_raises ActiveRecord::RecordInvalid do
+      Student.create!(first_name: "Ham", last_name: "Burger", school_email: "bhill@msudenver.edu", major: "CS", graduation_date: "2025-06-01")
+    end
+  end
+
+  # test 6
+  test "Should save a valid student" do
+    student = Student.new(first_name: "Hame", last_name: "Burger", school_email: "hburger@university.edu", major: "Mathematics", graduation_date: "2025-06-01")
+    assert student.save
+  end
+
+  # test 7
+  test "Should raise an error when the email format is invalid" do
+    invalid_email_student = Student.new(first_name: "Ham", last_name: "Burger", school_email: "email", major: "CS", graduation_date: "2025-06-01")
+    assert_not invalid_email_student.save
+    assert invalid_email_student.errors[:school_email].any?
+  end
+
+  # test 8
+  test "Should delete a student record" do
+    student = Student.create!(first_name: "Ham", last_name: "Burger", school_email: "hburger@university.edu", major: "CS", graduation_date: "2025-06-01")
+    assert_difference('Student.count', -1) do
+      student.destroy
+    end
+  end
 end
