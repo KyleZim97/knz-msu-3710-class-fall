b6dfbe5c9e025cd832de99a3cc38b0401e98ca40
Kyle Zimmerman <kzimme15@msudenver.edu>
2024-09-21 15:43:29 -0600

    Finished adding avatar pic logic for uploading, displaying, and default avatar pic

--- portfolio_app/app/models/student.rb
+++ portfolio_app/app/models/student.rb
@@ -5,7 +5,28 @@ class Student < ApplicationRecord
     validates :major, presence: true
     validates :graduation_date, presence: true
     validates :school_email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-    
+    validate :acceptable_image
 
-    #has_one_attached :avatar
+    has_one_attached :avatar
+
+    def avatar_url
+        if avatar.attached?
+            avatar
+        else
+            'portfolio_app\app\assets\images\default_image.jpg'  # Path to your default image
+        end
+    end
+
+    def acceptable_image
+        return unless avatar.attached?
+
+        unless avatar.blob.byte_size <= 1.megabyte
+            errors.add(:avatar, "is too big")
+        end
+        
+        acceptable_types = ["image/jpeg", "image/png"]
+        unless acceptable_types.include?(avatar.content_type)
+            errors.add(:avatar, "must be a JPEG or PNG")
+        end
+    end
 end
