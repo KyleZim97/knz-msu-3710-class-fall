f953f89cb1724e790f2896f085e30bf5b54bd469
Kyle Zimmerman <kzimme15@msudenver.edu>
2024-10-13 17:45:35 -0600

    Add nav bar. added search functionality. still working on m04

--- portfolio_app/app/assets/javascript/controllers/index.js
+++ portfolio_app/app/assets/javascript/controllers/index.js
@@ -1,6 +1,13 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
+//import { application } from "controllers/application"
+//import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+//import DropdownController from "./dropdown_controller"; // Import the dropdown controller
+
+//eagerLoadControllersFrom("controllers", application)
+
+// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 import DropdownController from "./dropdown_controller"; // Import the dropdown controller
 
-eagerLoadControllersFrom("controllers", application)
+application.register("dropdown", DropdownController); // Register the controller with Stimulus
+
